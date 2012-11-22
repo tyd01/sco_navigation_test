9acdc3d2101b7fb4d73fda0e88865e3b85cf7a22
michael luis <michael.luis.perso@gmail.com>
2012-11-22 18:04:28 +0100

    1st commit

---
+++ js/lmsCommunication - Copie.js
@@ -0,0 +1,126 @@
+﻿var scorm = pipwerks.SCORM;
+
+$( document ).ready( init );
+
+function init()
+{
+	scorm.version = "2004";
+	
+	connect();
+	
+	var learnerName = getValue( "cmi.learner_name" );
+	
+	$( "#Name" ).html( "Bonjour " + learnerName + " !!!" );
+	
+	$( "#goto1" ).click( goto1 );
+	$( "#goto2" ).click( goto2 );
+	$( "#goto3" ).click( goto3 );
+	
+	$( window ).unload( disconnect );
+}
+
+function goto1()
+{
+	if( getValue( "adl.nav.request_valid.choice.{target=page1}" ) == "true" )
+	{
+		setValue( 'adl.nav.request', '{target=page1}choice' );
+		disconnect();
+	}
+	
+	/* if( getValue( "adl.nav.request_valid.previous" ) == "true" )
+	{
+		setValue( 'adl.nav.request', 'previous' );
+		disconnect();
+	} */
+}
+
+function goto2()
+{
+	if( getValue( "adl.nav.request_valid.choice.{target=page2}" ) == "true" )
+	{
+		setValue( 'adl.nav.request', '{target=page2}choice' );
+		disconnect();
+	}
+	
+	/* if( getValue( "adl.nav.request_valid.continue" ) == "true" )
+	{
+		setValue( 'adl.nav.request', 'continue' );
+		disconnect();
+	} */
+}
+
+function goto3()
+{
+	/* if( getValue( "adl.nav.request_valid.choice.{target=page3}" ) == "true" )
+	{
+		setValue( 'adl.nav.request', '{target=page3}choice' );
+		disconnect();
+	} */
+	
+	if( getValue( "adl.nav.request_valid.continue" ) == "true" )
+	{
+		setValue( 'adl.nav.request', 'continue' );
+		setValue( 'adl.nav.request', 'continue' );
+		disconnect();
+	}
+}
+
+function connect()
+{
+	if( !scorm.connection.isActive )
+	{
+		var isSuccess = scorm.init();				
+		var msg = "Connexion : " + ( ( isSuccess )? "succès" : "échec" );
+		log( msg, isSuccess );
+	}
+}
+
+function disconnect()
+{
+	if( scorm.connection.isActive )
+	{
+		var isSuccess = scorm.quit();				
+		var msg = "Déconnexion : " + ( ( isSuccess )? "succès" : "échec" );
+		log( msg, isSuccess );
+	}
+}
+
+function save()
+{
+	if( scorm.connection.isActive )
+	{
+		var isSuccess = scorm.save();				
+		var msg = "Sauvegarde : " + ( ( isSuccess )? "succès" : "échec" );
+		log( msg, isSuccess );
+	}
+}
+
+function getValue( parameter )
+{
+	var value = null;
+	
+	if( scorm.connection.isActive )
+	{
+		value = scorm.get( parameter );
+		var msg = "Get " + parameter + " : " + ( ( value )? value : "échec" );
+		var	isSuccess = ( value )? true : false;
+		log( msg, isSuccess );
+	}
+	
+	return value;
+}
+
+function setValue( parameter, value )
+{
+	if( scorm.connection.isActive )
+	{
+		var isSuccess = scorm.set( parameter, value );
+		var msg = "Set " + parameter + " = " + value + " : " + ( ( isSuccess )? "succès" : "échec" );
+		log( msg, isSuccess );
+	}
+}
+
+function log( msg, isSuccess )
+{
+	$( "#console" ).append( "<div style=\"color:" + ( ( isSuccess )? "blue" : "orangered" ) + ";\">" + msg + "</div>" );
+}
---
+++ js/lmsCommunication.js
@@ -0,0 +1,170 @@
+﻿var API;
+var connectionIsActive = false;
+
+$( document ).ready( init );
+
+function init()
+{
+	$( window ).unload( disconnect );
+	
+	connect();
+	
+	$( "#Name" ).html( "Bonjour " + get( "cmi.learner_name" ) + " !!!" );
+	
+	get( "adl.nav.request_valid.choice.{target=page2}" );
+}
+
+function connect()
+{
+	API = pipwerks.SCORM.API.getHandle();
+	
+	if( API )
+	{
+		//init API communication
+		if( API.Initialize("") == "true" )
+		{
+			log( "Iniatialisation API ok" );
+			connectionIsActive = true;
+		}
+		else
+		{
+			var error = API.GetLastError();
+			var errorString = API.GetErrorString( error );
+			var diagnostic = API.GetDiagnostic( error );
+			
+			log( "Iniatialisation API erreur<br>" +
+				"error = " + error + " -> " + errorString + "<br>" +
+				"Diagnostic = " + diagnostic );
+		}
+	}
+}
+
+function disconnect()
+{
+	if( connectionIsActive )
+	{	
+		if( set( "cmi.exit", "normal" ) )
+		{
+			if( API.terminate("") == "true" )
+			{
+				log( "deconnection API ok" );
+				connectionIsActive = false;
+			}
+			else
+			{
+				var error = API.GetLastError();
+				var errorString = API.GetErrorString( error );
+				var diagnostic = API.GetDiagnostic( error );
+			
+				log( "deconnection API erreur<br>" +
+				"error = " + error + " -> " + errorString + "<br>" +
+				"Diagnostic = " + diagnostic );
+			}
+		}
+	}
+	else
+	{
+		log( "erreur deconnection : la connection est deja inactive" );
+	}
+}
+
+function get( parameter )
+{
+	var value = false;
+	
+	if( connectionIsActive )
+	{
+		var value = API.GetValue( parameter );
+		
+		if( value != "" )
+		{
+			log( "GetValue( " + parameter + " ) : " + value );
+		}
+		else
+		{
+			var error = API.GetLastError();
+			
+			if( error == "0" )
+			{
+				log( "GetValue( " + parameter + " ) : ''" );
+			}
+			else
+			{
+				var errorString = API.GetErrorString( error );
+				var diagnostic = API.GetDiagnostic( error );
+				
+				value = false;
+				
+				log( "GetValue( " + parameter + " ) : erreur<br>" + 
+					"error = " + error + " -> " + errorString + "<br>" +
+					"Diagnostic = " + diagnostic );
+			}
+		}
+	}
+	else
+	{
+		log( "erreur get : la connection est inactive" );
+	}
+	
+	return value;
+}
+
+function set( parameter, value )
+{
+	var success = false;
+	
+	if( connectionIsActive )
+	{
+		if( API.SetValue( parameter, value ) == "true" )
+		{
+			success = true;
+			log( "SetValue( " + parameter + ", " + value + " ) : ok" );
+		}
+		else
+		{
+			var error = API.GetLastError();
+			var errorString = API.GetErrorString( error );
+			var diagnostic = API.GetDiagnostic( error );
+			
+			log( "SetValue( " + parameter + ", " + value + " ) : erreur<br>" +
+			"error = " + error + " -> " + errorString + "<br>" +
+			"Diagnostic = " + diagnostic );
+		}
+	}
+	else
+	{
+		log( "erreur get : la connection est inactive" );
+	}
+	
+	return success;
+}
+
+function commit()
+{
+	if( connectionIsActive )
+	{
+		if( API.Commit( "" ) == "true" )
+		{
+			log( "Commit( '' ) : ok" );
+		}
+		else
+		{
+			var error = API.GetLastError();
+			var errorString = API.GetErrorString( error );
+			var diagnostic = API.GetDiagnostic( error );
+			
+			log( "Commit( '' ) : erreur<br>" +
+			"error = " + error + " -> " + errorString + "<br>" +
+			"Diagnostic = " + diagnostic );
+		}
+	}
+	else
+	{
+		log( "erreur commit : la connection est inactive" );
+	}
+}
+
+function log( msg )
+{
+	$( "#console" ).prepend( "<p>" + msg + "</p>" );
+}
